6d381e28eba643fb759fecc02214de30b22cf9d6
AnthonyKeith <52044937+AnthonyKeith@users.noreply.github.com>
2023-04-08 20:49:29 +0100

    Initial commit

---
+++ src/minihomework_4_pooa/MiniHomework_4_POOA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minihomework_4_pooa;
+
+/**
+ *
+ * @author USER
+ */
+public class MiniHomework_4_POOA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
